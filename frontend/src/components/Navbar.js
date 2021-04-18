a99f891ddc1c06a5026ebf3b6f9d905305b0a9f8
maizerocom <maizerocom@gmail.com>
2021-04-18 17:46:26 +0700

    tum Nav Nav Nav kub :shit:

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,58 +7,136 @@ const Navbar = (props) => {
   const { user, logout } = useAuthContext();
 
   return (
-    <nav className="nav flex flex-wrap items-center justify-between px-4 bg-transparent">
-      <Link
-        className="flex flex-no-shrink items-center mr-6 py-3 text-grey-darkest"
-        to="/"
-      >
-        <img
-          className="h-10 mr-2 w-10"
-          src="favicon.webp"
-          width="40px"
-          height="40px"
-          alt="logo"
-        />
-
-        <span className="font-semibold text-xl tracking-tight">
-          {user && `Welcome: ${user.username}`}
-        </span>
-      </Link>
-
-      <input className="menu-btn hidden" type="checkbox" id="menu-btn" />
-      <label
-        className="menu-icon block cursor-pointer md:hidden px-2 py-4 relative select-none"
-        htmlFor="menu-btn"
-      >
-        <span className="icon bg-grey-darkest flex items-center relative"></span>
-      </label>
-
-      <ul className="flex menu border-gray-500 border-b justify-end m-0 w-full md:border-none md:w-auto">
-        {user ? (
-          <Fragment>
-            {user.type === "AdminUser" ? (
+    <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-teal-500 mb-3">
+      <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+          <Link
+            className="flex flex-no-shrink items-center mr-6 py-3 text-grey-darkest"
+            to="/"
+          >
+            <img
+              className="h-10 mr-2 w-10"
+              src="favicon.webp"
+              width="40px"
+              height="40px"
+              alt="logo"
+            />
+          </Link>
+          {/* Hamburger Menu */}
+          {/* <button
+            className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+            type="button"
+          >
+            <span className="block relative w-6 h-px rounded-sm bg-white"></span>
+            <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+            <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+          </button> */}
+        </div>
+        <div className="lg:flex flex-grow items-center">
+          <ul className="flex flex-col lg:flex-row list-none ml-auto">
+            {user ? (
               <Fragment>
-                <Link to="/admin/products">admin products || </Link>
-                <Link to="/admin/product/create">create product || </Link>
-                <Link to="/admin/promotions">admin promotions || </Link>
-                <Link to="/admin/promotion/create">create promotion || </Link>
-                <Link to="/admin/orders">admin orders || </Link>
+                {user.type === "AdminUser" ? (
+                  <Fragment>
+                    <li className="nav-item">
+                      <Link
+                        to="/admin/products"
+                        className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      >
+                        admin products
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link
+                        to="/admin/product/create"
+                        className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      >
+                        create product
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link
+                        to="/admin/promotions"
+                        className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      >
+                        admin promotions
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link
+                        to="/admin/promotion/create"
+                        className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      >
+                        create promotion
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link
+                        to="/admin/orders"
+                        className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      >
+                        {/* <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i> */}
+                        admin orders
+                      </Link>
+                    </li>
+                  </Fragment>
+                ) : (
+                  <Fragment>
+                    <li className="nav-item">
+                      <Link
+                        to="/products"
+                        className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      >
+                        {/* <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i> */}
+                        products
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link
+                        to="/promotions"
+                        className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      >
+                        {/* <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i> */}
+                        promotions
+                      </Link>
+                    </li>
+                  </Fragment>
+                )}
+                <li className="nav-item">
+                  <Link
+                    onClick={logout}
+                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  >
+                    {/* <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i> */}
+                    Logout
+                  </Link>
+                </li>
               </Fragment>
             ) : (
               <Fragment>
-                <Link to="/products">products || </Link>
-                <Link to="/promotions">promotions || </Link>
+                <li className="nav-item">
+                  <Link
+                    to="/register"
+                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  >
+                    {/* <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i> */}
+                    Register
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    to="/login"
+                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  >
+                    {/* <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i> */}
+                    Login
+                  </Link>
+                </li>
               </Fragment>
             )}
-            <button onClick={logout}>Logout</button>
-          </Fragment>
-        ) : (
-          <Fragment>
-            <Link to="/register">Register</Link>
-            <Link to="/login">Login</Link>
-          </Fragment>
-        )}
-      </ul>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
