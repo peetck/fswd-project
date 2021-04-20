672acb45b157a64229d9d859cd45d6163ffb8341
skaferun9 <43022322+skaferun9@users.noreply.github.com>
2021-04-20 19:36:20 +0700

    update admin frontend

--- frontend/src/pages/admin/AdminCreateProduct.js
+++ frontend/src/pages/admin/AdminCreateProduct.js
@@ -54,37 +54,120 @@ const AdminCreateProduct = () => {
     // </div>
 
     <form onSubmit={handleSubmit}>
-      Title:{" "}
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        className="border"
-      />
-      Description:{" "}
-      <input
-        type="text"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        className="border"
-      />
-      Price:
-      <input
-        type="text"
-        value={price}
-        onChange={(e) => setPrice(e.target.value)}
-        className="border"
-      />
-      Quantity:{" "}
-      <input
-        type="text"
-        value={quantity}
-        onChange={(e) => setQuantity(e.target.value)}
-        className="border"
-      />
-      <input type="file" multiple onChange={(e) => setImages(e.target.files)} />
-      <input type="submit" value="Create Normal Product" />
+
+      <div className="bg-white shadow rounded-lg p-6 m-5 mt-7">
+
+        <div className="grid lg:grid-cols-2 gap-6">
+          <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+            <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+              <p>
+                <label for="name" className="bg-white text-gray-600 px-1">Title *</label>
+              </p>
+            </div>
+            <p>
+              <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className="py-1 px-1 text-gray-900 outline-none block h-full w-full"
+              />
+
+            </p>
+          </div>
+          <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+            <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+              <p>
+                <label for="lastname" className="bg-white text-gray-600 px-1">Description *</label>
+              </p>
+            </div>
+            <p>
+              <input
+                type="text"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className="py-1 px-1 outline-none block h-full w-full"
+              />
+
+            </p>
+          </div>
+          <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+            <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+              <p>
+                <label for="username" className="bg-white text-gray-600 px-1">Price *</label>
+              </p>
+            </div>
+            <p>
+              <div class="flex flex-row">
+                <span class="flex items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold text-grey-darker">$</span>
+                <input
+                  type="text"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                  className="py-1 px-1 outline-none block h-full w-full"
+                />
+              </div>
+            </p>
+          </div>
+          <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+            <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+              <p>
+                <label for="password" className="bg-white text-gray-600 px-1">Quantity *</label>
+              </p>
+            </div>
+            <p>
+              <input
+                type="text"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+                className="py-1 px-1 outline-none block h-full w-full"
+              />
+
+            </p>
+          </div>
+
+          <input className="" type="file" multiple onChange={(e) => setImages(e.target.files)} />
+
+
+
+          <input className="bg-gray-700 p-3 text-white" type="submit" value="Create Normal Product" />
+        </div>
+
+      </div>
     </form>
+    //   <form onSubmit={handleSubmit}>
+    //   Title:{" "}
+    // <input
+    //   type="text"
+    //   value={title}
+    //   onChange={(e) => setTitle(e.target.value)}
+    //   className="border"
+    // />
+    // Description:{" "}
+    // <input
+    //   type="text"
+    //   value={description}
+    //   onChange={(e) => setDescription(e.target.value)}
+    //   className="border"
+    // />
+    // Price:
+    // <input
+    //   type="text"
+    //   value={price}
+    //   onChange={(e) => setPrice(e.target.value)}
+    //   className="border"
+    // />
+    // Quantity:{" "}
+    // <input
+    //   type="text"
+    //   value={quantity}
+    //   onChange={(e) => setQuantity(e.target.value)}
+    //   className="border"
+    // />
+    // <input type="file" multiple onChange={(e) => setImages(e.target.files)} />
+    // <input type="submit" value="Create Normal Product" />
+    // </form> 
+
+
   );
 };
 
--- frontend/src/pages/admin/AdminCreatePromotion.js
+++ frontend/src/pages/admin/AdminCreatePromotion.js
@@ -35,32 +35,85 @@ const AdminCreatePromotion = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <select
-        value={product}
-        defaultValue="default"
-        onChange={(e) => {
-          setProduct(e.target.value);
-        }}
-      >
-        <option value="default">--select--</option>
-        {products?.normalProducts.map((product) => (
-          <option value={product._id} key={product._id}>
-            {product.title}
-          </option>
-        ))}
-      </select>
+      <div className="bg-white shadow rounded-lg p-6 m-5 mt-7">
 
-      <p>
-        Percent:{" "}
-        <input
-          type="text"
-          value={percent}
-          onChange={(e) => setPercent(e.target.value)}
-        />
-      </p>
-
-      <input type="submit" value="Create Promotion" />
+        <div className="grid lg:grid-cols-2 gap-6">
+          <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+            <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+              <p>
+                <label for="name" className="bg-white text-gray-600 px-1">Title *</label>
+              </p>
+            </div>
+            <p>
+              <select
+                className="border-outline p-2 rounded w-full"
+                value={product}
+                defaultValue="default"
+                onChange={(e) => {
+                  setProduct(e.target.value);
+                }}
+              >
+                <option value="default">--select--</option>
+                {products?.normalProducts.map((product) => (
+                  <option value={product._id} key={product._id}>
+                    {product.title}
+                  </option>
+                ))}
+              </select>
+            </p>
+          </div>
+          <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+            <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+              <p>
+                <label for="username" className="bg-white text-gray-600 px-1">Percent *</label>
+              </p>
+            </div>
+            <p>
+              <div class="flex flex-row mt-1">
+                <span class="flex items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold text-grey-darker">%</span>
+                <input
+                  type="text"
+                  value={percent}
+                  onChange={(e) => setPercent(e.target.value)}
+                  className="py-1 px-1 outline-none block h-full w-full"
+                />
+              </div>
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center mt-8">
+          <input type="submit" value="Create Promotion" className="bg-gray-700 p-3 text-white w-3/4" />
+        </div>
+      </div>
     </form>
+    // <form onSubmit={handleSubmit}>
+    //   <select
+    //   className="border p-2 rounded"
+    //     value={product}
+    //     defaultValue="default"
+    //     onChange={(e) => {
+    //       setProduct(e.target.value);
+    //     }}
+    //   >
+    //     <option value="default">--select--</option>
+    //     {products?.normalProducts.map((product) => (
+    //       <option value={product._id} key={product._id}>
+    //         {product.title}
+    //       </option>
+    //     ))}
+    //   </select>
+
+    //   <p>
+    //     Percent:{" "}
+    //     <input
+    //       type="text"
+    //       value={percent}
+    //       onChange={(e) => setPercent(e.target.value)}
+    //     />
+    //   </p>
+
+    //   <input type="submit" value="Create Promotion" />
+    // </form>
   );
 };
 
