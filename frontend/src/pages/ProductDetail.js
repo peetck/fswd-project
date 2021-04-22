bfa9a39954ba25cb609da131f24c539b00b21dc0
maizerocom <maizerocom@gmail.com>
2021-04-22 23:48:34 +0700

    :lipstick: Update product detail <3

--- frontend/src/pages/ProductDetail.js
+++ frontend/src/pages/ProductDetail.js
@@ -136,26 +136,50 @@ const ProductDetail = () => {
                         prod.quantity > 0
                       ) {
                         return (
-                          <h1
+                          <button
+                            className={`${
+                              color === selectedColor
+                                ? "border-indigo-600"
+                                : "border-gray-200"
+                            } bg-white hover:bg-gray-100 text-sm text-gray-800 font-semibold py-2 px-5 shadow border focus:outline-none mr-1 flex relative`}
                             onClick={() =>
                               color === selectedColor
                                 ? setSelectedColor()
                                 : setSelectedColor(color)
                             }
-                            className={
-                              color === selectedColor
-                                ? "text-blue-600"
-                                : "text-black"
-                            }
                           >
-                            {color} -{" "}
-                          </h1>
+                            {color}
+                            {color === selectedColor ? (
+                              <div className="absolute right-0 bottom-0">
+                                <svg
+                                  className="h-4 w-4"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke="#4f46e5"
+                                  x="0"
+                                  y="0"
+                                >
+                                  <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M5 13l4 4L19 7"
+                                  />
+                                </svg>
+                              </div>
+                            ) : (
+                              ""
+                            )}
+                          </button>
                         );
                       }
                     }
                     return (
-                      <button disabled className="text-gray-400">
-                        {color} -{" "}
+                      <button
+                        disabled
+                        className="bg-white text-sm text-gray-400 font-semibold py-2 px-5 rounded shadow border border-gray-200 mr-1"
+                      >
+                        {color}
                       </button>
                     );
                   })}
@@ -175,25 +199,49 @@ const ProductDetail = () => {
                       ) {
                         return (
                           <button
+                            className={`${
+                              size === selectedSize
+                                ? "border border-indigo-600"
+                                : "border border-white"
+                            } bg-white hover:bg-gray-100 text-sm text-gray-800 font-semibold py-2 px-5 shadow  focus:outline-none mr-1 flex relative`}
                             onClick={() =>
                               size === selectedSize
                                 ? setSelectedSize()
                                 : setSelectedSize(size)
                             }
-                            className={
-                              size === selectedSize
-                                ? "text-blue-600"
-                                : "text-black"
-                            }
                           >
-                            {size} -{" "}
+                            {size}
+                            {size === selectedSize ? (
+                              <div className="absolute right-0 bottom-0">
+                                <svg
+                                  className="h-4 w-4"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke="#4f46e5"
+                                  x="0"
+                                  y="0"
+                                >
+                                  <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M5 13l4 4L19 7"
+                                  />
+                                </svg>
+                              </div>
+                            ) : (
+                              ""
+                            )}
                           </button>
                         );
                       }
                     }
                     return (
-                      <button disabled className="text-gray-400">
-                        {size} -{" "}
+                      <button
+                        disabled
+                        className="bg-white text-sm text-gray-400 font-semibold py-2 px-5 rounded shadow border border-gray-200 mr-1"
+                      >
+                        {size}
                       </button>
                     );
                   })}
