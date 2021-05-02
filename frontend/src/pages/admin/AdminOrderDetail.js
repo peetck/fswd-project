5b712672a8e95730bbbf78a52fbe3c8a8e6f25f5
skaferun9 <43022322+skaferun9@users.noreply.github.com>
2021-05-03 02:55:25 +0700

    Update AdminOrderDetail Page

--- frontend/src/pages/admin/AdminOrderDetail.js
+++ frontend/src/pages/admin/AdminOrderDetail.js
@@ -1,14 +1,13 @@
-
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
 import { v4 as uuidv4 } from "uuid";
 import moment from "moment";
-import Loader from "../../components/Loader";
 
-import { Link } from "react-router-dom";
-import { useQuery, gql, useMutation } from "@apollo/client";
-import Truncate from "react-truncate";
+
+import Loader from "../../components/Loader";
 import CartItem from "../../components/CartItem";
+import Input from "../../components/Input";
 
 
 const ORDER_QUERY = gql`
@@ -54,100 +53,118 @@ const AdminOrderDetail = () => {
 
   console.log(data)
   return (
-    <div className="">
+    <div className="flex flex-col mx-8">
+      <div className="container mx-auto my-7 min-w-min">
+        <div className="flex flex-col lg:flex-row ">
+          <div className="w-full lg:w-3/4 bg-white px-10 py-10">
+            <div className="flex justify-between border-b pb-8">
+              <h1 className="font-bold uppercase text-2xl">
+                Order no: {orderId}
+              </h1>
+              <h2 className="font-bold text-2xl uppercase hidden sm:inline-block">
+                {data.order.products.length} Items
+              </h2>
+            </div>
+            <div className="flex justify-center mt-10 mb-5 -mx-8 px-6">
+              <h3 className="font-bold text-gray-600 text-xs uppercase w-2/5">
+                Product
+              </h3>
+              <h3 className="font-bold text-gray-600 text-xs uppercase w-1/5 text-center hidden lg:block">
+                Unit Price
+              </h3>
+              <h3 className="font-bold text-gray-600 text-xs uppercase w-2/5 text-center lg:w-1/5">
+                Amount
+              </h3>
+              <h3 className="font-bold text-gray-600 text-xs uppercase w-1/5 text-center ">
+                Item Subtotal
+              </h3>
+            </div>
 
-      <div className="flex justify-center mt-10">
-        <div className="flex flex-col ml-2 pt-4 px-6">
-          <div className="bg-coolGray-500 lg:order-1 lg:row-span-1 2xl:row-span-1 lg:col-span-2 rounded-lg shadow-xl mb-5 lg:mb-0 p-5 pt-8">
-            <div className="mx-6 my-3 2xl:mx-10 mb-10">
-             <center >
-              <h1 className="text-white text-xl">{orderId}</h1>
-             </center>
-              {/* <div>
-                <div className="flex justify-between pb-6 text-sm uppercase mt-11 items-end">
-                  <h1 className="flex items-center text-lg">
-                    {moment(data.order.createdAt).format("ll")}
-                  </h1>
-                  <h1 className="flex items-center text-lg">
-                    {moment(data.order.createdAt).format("LT")}
-                  </h1>
+            {data.order.products.map((product) => (
+              <CartItem
+                key={uuidv4()}
+                title={product.title}
+                color={product.color}
+                price={product.price}
+                type={product.type}
+                quantity={product.quantity}
+                priceAfterDiscount={product.priceAfterDiscount}
+                size={product.size}
+                imageUrl={product.imageUrl}
+              />
+            ))}
+          </div>
+          <div className="w-full lg:w-1/4 px-8 py-10">
+            <h1 className="font-bold text-2xl border-b pb-8 uppercase">
+              Information
+            </h1>
+
+            <div className="text-center my-8 uppercase border-b pb-8 text-royal-blue text-4xl">
+              ฿{data.order.totalPrice}
+            </div>
+
+            <div
+              className={`text-center text-4xl my-8 uppercase ${data.order.deliveryStatus === "Waiting"
+                ? "text-yellow-500"
+                : "text-green-600"
+                }`}
+            >
+              {data.order.deliveryStatus}
+            </div>
+
+            <div className="flex flex-col justify-between mb-5 border-t mt-8">
+              <span className="font-bold text-sm uppercase mt-8">
+                Payment Method
+              </span>
+              <div className="mt-6">
+                <div className="flex items-center justify-between w-full bg-white rounded-md border p-4 focus:outline-none">
+                  <label className="flex items-center">
+                    <span className="ml-2 text-sm text-gray-700">
+                      {data.order.paymentMethod === "CreditCard"
+                        ? "Credit Card"
+                        : "Cash on Delivery"}
+                    </span>
+                  </label>
+
+                  <span className="material-icons">
+                    {data.order.paymentMethod === "CreditCard"
+                      ? "credit_card"
+                      : "paid"}
+                  </span>
                 </div>
               </div>
-              */}
             </div>
-            <div className="-mt-6 relative">
-              <p className="text-green-400 text-xl 2xl:text-4xl font-bold px-7 lg:px-9 2xl:pt-6 2xl:mx-2">{data.order.deliveryStatus}</p>
-              <br />
-              <p className="text-white md:text-sm 2xl:text-3xl px-7 lg:px-9 mb-3 2xl:pb-8 2xl:mx-2">ORDER TOTAL :{data.order.totalPrice}</p>
 
-              <br />
-              <p className="text-white  font-medium md:text-sm 2xl:text-3xl px-7 lg:px-9 mb-3 2xl:pb-8 2xl:mx-2">SHIPPING ADDRESS : {data.order.deliveryAddress}</p>
-              <br />
-              <p className="text-white  font-medium md:text-sm 2xl:text-3xl px-7 lg:px-9 mb-3 2xl:pb-8 2xl:mx-2">PAYMENT METHOD : {data.order.paymentMethod
-              }</p>
+            <div className="flex flex-col justify-between mb-5 border-t mt-8">
+              <span className="font-bold text-sm uppercase mt-8">
+                Delivery Address
+              </span>
+
+              <div className="mt-6">
+                <Input
+                  name="address"
+                  value={data.order.deliveryAddress}
+                  rows={3}
+                  type="textarea"
+                  disabled
+                />
+              </div>
+            </div>
+
+            <div className="border-t mt-8">
+              <div className="flex justify-between pb-6 text-sm uppercase mt-11 items-end">
+                <h1 className="flex items-center text-lg">
+                  {moment(data.order.createdAt).format("ll")}
+                </h1>
+                <h1 className="flex items-center text-lg">
+                  {moment(data.order.createdAt).format("LT")}
+                </h1>
+              </div>
             </div>
           </div>
         </div>
       </div>
-              
-      <div className="flex justify-center  ">
-        
-
-        <div className="w-full lg:w-3/4 bg-white px-10 py-10 ">
-        <div className="flex justify-between pb-6 text-sm uppercase mt-11 items-end border-b border-black">
-                  <h1 className="flex items-center text-lg">
-                    {moment(data.order.createdAt).format("ll")}
-                  </h1>
-                  <h1 className="flex items-center text-lg">
-                    {moment(data.order.createdAt).format("LT")}
-                  </h1>
-                </div>
-
-          <div className="flex justify-center mt-10 mb-5 -mx-8 px-6 ">
-            <h3 className="font-bold text-gray-600 text-xs uppercase w-2/5">
-              Product
-              </h3>
-            <h3 className="font-bold text-gray-600 text-xs uppercase w-1/5 text-center hidden lg:block">
-              Unit Price
-              </h3>
-            <h3 className="font-bold text-gray-600 text-xs uppercase w-2/5 text-center lg:w-1/5">
-              Amount
-              </h3>
-            <h3 className="font-bold text-gray-600 text-xs uppercase w-1/5 text-center ">
-              Item Subtotal
-              </h3>
-          </div>
-
-          {data.order.products.map((product) => (
-            <CartItem
-              key={uuidv4()}
-              title={product.title}
-              color={product.color}
-              price={product.price}
-              type={product.type}
-              quantity={product.quantity}
-              priceAfterDiscount={product.priceAfterDiscount}
-              size={product.size}
-              imageUrl={product.imageUrl}
-            />
-          ))}
-
-
-
-
-
-
-
-
-
-        </div>
-
-
-      </div>
-
-
     </div>
-
   );
 };
 
